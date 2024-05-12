a3716ad07fa69f87b7b890b6d0b3e1c76e58ea33
AlinaSun0201 <131934395+AlinaSun0201@users.noreply.github.com>
2024-05-12 22:12:16 +0300

    add files

--- src/main/java/HelloITGirls.java
+++ src/main/java/HelloITGirls.java
@@ -1,5 +1,102 @@
+import java.util.Date;
+
 public class HelloITGirls {
     public static void main(String[] args) {
-        System.out.println("Hello It girls!");
+
+
+       String str = "Training Java";
+        System.out.println(str.length());
+        System.out.println(str.substring(6,7));
+        String date = String.valueOf(new Date());
+        String newStr = str + date;
+        System.out.println(newStr);
+//        int b = 50;
+//        int c = 20;
+//        ++b;
+//        --c;
+//        System.out.println(b >= 51);
+//        System.out.println(c <= 22);
+
+//        float side1 = 5;
+//        float side2 = 8;
+//
+//        double area = (side1 * side2) / 2;
+//        double side3 = Math.sqrt( Math.pow(side1,2) + Math.pow(side2,2) );
+//
+//        double perimetr = side1 + side2 + side3;
+//
+//        System.out.println("Площадь = " + area);
+//        System.out.println("Периметр = " + perimetr);
+//
+//        if (perimetr > 15) {
+//                System.out.println("Периметр > 15 ");
+//            }
+//        if (area < 30) {
+//            System.out.println("Площадь < 30 ");
+//        }
+
+
+//        System.out.println("Hello It girls!");
+
+//        byte itsByte = 129;
+//        short itsShort = -32999;
+//        int itsInteger = -234;
+//        long itsLong = 9387148719479817L;
+//        float itsFloat = 21328493841.24;
+//        double itsDouble = 3296543.2;
+//        char itsChar = 'a';
+//        boolean itsBoolean = true;
+//
+//        System.out.println(itsByte);
+//        System.out.println(itsShort);
+//        System.out.println(itsInteger);
+//        System.out.println(itsLong);
+//        System.out.println(itsFloat);
+//        System.out.println(itsDouble);
+//        System.out.println(itsChar);
+//        System.out.println(itsBoolean);
+//        char symbol1 = 1099;
+//        char symbol2 = 'ы';
+//        char symbol3 = '\u044B';
+//        System.out.println(symbol1);
+//        System.out.println(symbol2);
+//        System.out.println(symbol3);
+//        String str = "Hello";
+//        String str2 = new String("How are you?");
+//        System.out.println(str);
+//        System.out.println(str2);
+
+//        byte itsByte = 123;
+//        Byte boxed1;
+//        boxed1 = new Byte(itsByte);
+//        itsByte = boxed1.byteValue();
+//        short itsShort = 2345;
+//        Short boxed2;
+//        boxed2 = Short.valueOf(itsShort);
+//        itsShort = boxed2.shortValue();
+//        int itsInteger = 123;
+//        Integer boxed3;
+//        boxed3 = itsInteger;
+//        itsInteger = boxed3.intValue();
+//        long itsLong = 1234567L;
+//        Long boxed4;
+//        boxed4 = new Long(itsLong);
+//        itsLong = boxed4.longValue();
+//        float itsFloat = 1234.23F;
+//        Float boxed5;
+//        boxed5 = itsFloat;
+//        itsFloat = boxed5.floatValue();
+//        double itsDouble= 1234.23;
+//        Double boxed6;
+//        boxed6 = itsDouble;
+//        itsDouble = boxed6.doubleValue();
+//        char itsChar = 'a';
+//        Character boxed7;
+//        boxed7 = Character.valueOf(itsChar);
+//        itsChar = boxed7.charValue();
+//        boolean t = true;
+//        Boolean boxed8;
+//        boxed8 = Boolean.valueOf(t);
+//        t = boxed8.booleanValue();
     }
 }
